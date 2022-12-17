f83055fbcdd97ab66fb697e461ddbea8c7668d67
Isomukhammad <s.isomuxammad@gmail.com>
2022-12-17 17:37:36 +0500

    .

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,5 +1,7 @@
 export const getData = async (url) => {
-  const data = await fetch(`https://api.ubdgroup.uz/api/v2/${url}`);
+  const data = await fetch(`https://api.ubdgroup.uz/api/v2/${url}`, {
+    mode: "cors",
+  });
   const res = await data.json();
   return res;
 };
