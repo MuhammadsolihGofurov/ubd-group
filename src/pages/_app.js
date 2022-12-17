644bdb8672256933cb23dfa259466598796ca861
Isomukhammad <s.isomuxammad@gmail.com>
2022-12-17 16:31:13 +0500

    Trying to fix insecure problem

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import Header from "../components/header/header.component";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </div>
