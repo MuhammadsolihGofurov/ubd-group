e380baea199de4058c0182d3b1de6af06515e9a8
Isomukhammad <s.isomuxammad@gmail.com>
2022-12-17 17:35:56 +0500

    Testing HTTPS request

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,5 +1,5 @@
 export const getData = async (url) => {
-  const data = await fetch(`http://api.ubdgroup.uz/api/v2/${url}`);
+  const data = await fetch(`https://api.ubdgroup.uz/api/v2/${url}`);
   const res = await data.json();
   return res;
 };
