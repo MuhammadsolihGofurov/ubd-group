a37f6c5e8189ef44ff644fe23dc40ae192247948
Isomukhammad <s.isomuxammad@gmail.com>
2022-12-17 17:25:33 +0500

    Fixing fetching

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,9 @@
-import Head from "next/head";
 import Header from "../components/header/header.component";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <Head>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        ></meta>
-      </Head>
       <Header />
       <Component {...pageProps} />
     </div>
