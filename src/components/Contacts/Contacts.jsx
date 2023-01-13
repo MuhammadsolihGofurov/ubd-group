0013bc70193b8a21f5bf40f683e625e512aa2a13
Isomukhammad <s.isomuxammad@gmail.com>
2023-01-13 12:13:17 +0500

    fix: adding contacts form

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,29 +1,42 @@
-import CompanyContacts from "../company-contacts/company-contact.component";
-import ContactForm from "../contact-form/contact-form.component";
-
-import styled from "styled-components";
-import styles from './Contects.module.scss'
+import styles from './Contacts.module.scss'
 
 const Contacts = () => (
     <div className={styles.container}>
-        <h3>Наши Контакты <span>Контакты</span></h3>
-        <div className={styles.contacts}>
-            <div>
-                <h4>Телефон:</h4>
-                <p>+998 97 703-70-80</p>
-            </div>
-            <div>
-                <h4>Почта:</h4>
-                <p>info@ubdgroup.uz</p>
+        <div className={styles.content}>
+            <h3>Наши Контакты <span>Контакты</span></h3>
+            <div className={styles.contacts}>
+                <div>
+                    <h4>Телефон:</h4>
+                    <p>+998 97 703-70-80</p>
+                </div>
+                <div>
+                    <h4>Почта:</h4>
+                    <p>info@ubdgroup.uz</p>
 
+                </div>
+                <div>
+                    <h4>Адрес:</h4>
+                    <p>г.Ташкент, ул.Мустакиллик 59</p>
+                    <p>Показать на карте</p>
+                </div>
             </div>
-            <div>
-                <h4>Адрес:</h4>
-                <p>г.Ташкент, ул.Мустакиллик 59</p>
-                <p>Показать на карте</p>
-            </div>
+            <form>
+                <input type="text" placeholder="Ваше имя" />
+                <input type="text" placeholder="Ваш E-mail" />
+                <textarea name="" id="" cols="30" rows="10" placeholder="Ваше сообщение"></textarea>
+                <div className={styles.button}>
+                    <button>
+                        <svg
+                            viewBox="0 0 24 24"
+                            width={24}
+                            height={24}
+                        >
+                            <use xlinkHref="#arrow-outward"></use>
+                        </svg>
+                    </button>
+                </div>
+            </form>
         </div>
-        <ContactForm />
         <div className={styles.eclipse} />
     </div>
 )
