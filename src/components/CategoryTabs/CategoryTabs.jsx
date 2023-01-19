c341786173d4a542e66ff7eaa2348d78f8b83808
Isomukhammad <s.isomuxammad@gmail.com>
2023-01-19 12:16:32 +0500

    feat: screen width function

--- src/components/CategoryTabs/CategoryTabs.jsx
+++ src/components/CategoryTabs/CategoryTabs.jsx
@@ -1,53 +1,67 @@
+import { useEffect, useRef, useState } from 'react';
+
 import data from '../../../db.json';
 
 import ImageComponent from '../ImageComponent/ImageComponent'
 
-import styles from './CategoryTabs.module.scss';
-import { useEffect, useRef } from 'react';
-
 import { gsap } from 'gsap/dist/gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
+import styles from './CategoryTabs.module.scss';
+
 const CategoryTabs = () => {
+    const [isMobile, setIsMobile] = useState(false)
     const categoryTabsRef = useRef(null);
+
+    const handleResize = () => {
+        if (window.innerWidth < 1025) {
+            setIsMobile(true)
+        } else {
+            setIsMobile(false)
+        }
+    }
 
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
+        if (!isMobile) {
+            const cards = gsap.utils.toArray(".card");
+            const spacer = 20;
+            const minScale = 0.8;
 
-        const cards = gsap.utils.toArray(".card");
-        const spacer = 20;
-        const minScale = 0.8;
+            const distributor = gsap.utils.distribute({ base: minScale, amount: 0.2 });
 
-        const distributor = gsap.utils.distribute({ base: minScale, amount: 0.2 });
+            cards.forEach((card, index) => {
+                const scaleVal = distributor(index, cards[index], cards);
 
-        cards.forEach((card, index) => {
-            const scaleVal = distributor(index, cards[index], cards);
+                const tween = gsap.to(card, {
+                    scrollTrigger: {
+                        trigger: card,
+                        start: `top top`,
+                        scrub: true,
+                        markers: false,
+                        invalidateOnRefresh: true
+                    },
+                    ease: "none",
+                    scale: scaleVal
+                });
 
-            const tween = gsap.to(card, {
-                scrollTrigger: {
+                ScrollTrigger.create({
                     trigger: card,
-                    start: `top top`,
-                    scrub: true,
+                    start: `top-=${index * spacer} top`,
+                    endTrigger: '.cards',
+                    end: `bottom top+=${250 + (cards.length * spacer)}`,
+                    pin: true,
+                    pinSpacing: false,
                     markers: false,
-                    invalidateOnRefresh: true
-                },
-                ease: "none",
-                scale: scaleVal
+                    id: 'pin',
+                    invalidateOnRefresh: true,
+                });
             });
+        }
 
-            ScrollTrigger.create({
-                trigger: card,
-                start: `top-=${index * spacer} top`,
-                endTrigger: '.cards',
-                end: `bottom top+=${250 + (cards.length * spacer)}`,
-                pin: true,
-                pinSpacing: false,
-                markers: false,
-                id: 'pin',
-                invalidateOnRefresh: true,
-            });
-        });
+        window.addEventListener("resize", handleResize)
     });
+
 
     return (
         <div className={styles.container} ref={categoryTabsRef} id="gallary">
